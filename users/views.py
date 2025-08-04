bfde72026f29ce73547a03616a31f663ec0b8df2
Aditya-7010 <adisharma70100@gmail.com>
2025-08-04 19:34:50 +0530

    Added user django registeration form Added superuser for the admin Changed users/views.py to add registeration form logic

--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def register(request):
-    return render(request, "users/register.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            login(request, form.save())
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, "users/register.html", {'form': form})
 
 def login(request):
     return render(request, "users/login.html")
